497a96e6a67c883f3beb1d39ca5a2cb5f4a26e79
valdidev <valdicode.fernando@gmail.com>
2022-09-25 13:52:12 +0200

    Transiciones y sonido de encendido

---
+++ main.js
@@ -0,0 +1,8 @@
+const gameboy = document.querySelector('#gameboy');
+const sonidoEncendido = document.querySelector('#sonido-encendido');
+
+gameboy.addEventListener('mouseenter', () => {
+    setTimeout(() => {
+        sonidoEncendido.play();
+    }, 3500)
+})
